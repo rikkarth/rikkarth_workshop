c742e9175e16abcf94135432f4bc1f799ce2e9bc
rikkarth <ricardominamendes@outlook.com>
2022-07-20 23:07:00 +0100

    code cleanup

--- src/script.js
+++ src/script.js
@@ -1,49 +1,43 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".wrap", {
-
-    scrollTrigger: {
-        trigger: ".wrap",
-        start: "top top",
-        end: "+=50%",
-        scrub: 2,
-        pin: ".container",
-        pinSpacing: false,
-        // markers: true,
-    },
-
+  scrollTrigger: {
+    trigger: ".wrap",
+    start: "top top",
+    end: "+=50%",
+    scrub: 2,
+    pin: ".container",
+    pinSpacing: false,
+    // markers: true,
+  },
 });
 
-
 let tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".wrap",
-        start: 'top top',
-        end: '+=50%',
-        scrub: 1,
-        pin: true,
-        
-        // markers: true,
-    },
-})
+  scrollTrigger: {
+    trigger: ".wrap",
+    start: "top top",
+    end: "+=50%",
+    scrub: 1,
+    pin: true,
 
-tl
-    .from('.video-1', {
-        
-        autoAlpha: 1,
-    })
-    .from('.video-2', {
-        autoAlpha: 0,
-    });
+    // markers: true,
+  },
+});
+
+tl.from(".video-1", {
+  autoAlpha: 1,
+}).from(".video-2", {
+  autoAlpha: 0,
+});
 
 gsap.to("#watermark_me", {
-    scrollTrigger: {
-        trigger: "#watermark_me",
-        start: "top 100px",
-        scrub: 1,
-        toggleActions: "restart pause reverse pause"
-    },
-    x: 450,
-    rotation: 360,
-    duration: 10
+  scrollTrigger: {
+    trigger: "#watermark_me",
+    start: "top 100px",
+    scrub: 1,
+    toggleActions: "restart pause reverse pause",
+  },
+  x: 450,
+  rotation: 360,
+  duration: 10,
 });
